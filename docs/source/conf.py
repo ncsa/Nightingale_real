87344ecec3ccda3834523c258c4b2869a608bb90
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2023-09-12 15:13:05 -0500

    Update conf.py added NCSA logo
    added NCSA logo to display in menu pane

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,3 +38,11 @@ epub_show_urls = 'footnote'
 html_css_files = [
     'css/custom.css',
 ]
+
+# -- Logo 
+html_static_path = ['_static']
+html_logo = "images/BlockI-NCSA-Full-Color-RGB_border4.png"
+html_theme_options = {
+     'logo_only': False,
+     'display_version': False,
+ }
